3c65655218076cfce60a05ca0113a454aa3306c4
Amrutha Nagaraj <79809556+amruthanag@users.noreply.github.com>
2021-11-10 11:21:35 -0600

    Add files via upload

---
+++ 182-Duplicate Emails.sql
@@ -0,0 +1,22 @@
+/*Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+*/
+/*SOLUTION*/
+
+SELECT Email
+FROM PERSON
+GROUP BY Email
+HAVING COUNT(1) > 1;
