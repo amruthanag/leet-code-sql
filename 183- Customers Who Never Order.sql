ac0022acd1445d7e637fa7b0ef835820500143e0
Amrutha Nagaraj <79809556+amruthanag@users.noreply.github.com>
2021-11-10 11:16:48 -0600

    Add files via upload

---
+++ 183- Customers Who Never Order.sql
@@ -0,0 +1,34 @@
+/*Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
+/*SOLUTION*/
+
+SELECT Customers.name AS Customers
+FROM Customers
+LEFT JOIN Orders
+ON Customers.Id = Orders.CustomerId
+WHERE Orders.CustomerId IS NULL ;
