ff3c54d709a4dceac1d1eab5690c5bb1e81e6086
Amrutha Nagaraj <79809556+amruthanag@users.noreply.github.com>
2023-09-26 19:24:16 -0700

    Update Deleteduplicateemail.sql

--- Deleteduplicateemail.sql
+++ Deleteduplicateemail.sql
@@ -1,2 +1,19 @@
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+
 delete p1 from person p1,person p2 
 where p1.email=p2.email and p1.id>p2.id;
